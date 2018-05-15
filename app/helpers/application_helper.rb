2dbe99f97aa06451d83685ac501ebed627f63cb0
kuroneko9963 <kuroneko9963@gmail.com>
2018-05-16 01:50:11 +0900

    静的ページの追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def get_title(page_name = '')
+    basic_title = 'Memoria'
+    if page_name.empty?
+      basic_title
+    else
+      "#{page_name} | #{basic_title}"
+    end
+  end
 end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @basic_title = "Memoria"
+  end
+
+  test "should get top" do
+    get root_path
+    assert_response :success
+    assert_select 'title', "#{@basic_title}"
+    assert_select 'a[href=?]', root_path
+    assert_select 'h1', 'Welcome to Memoria!'
+    assert_select 'a.top-btn[href=?]', '#'
+  end
+end
